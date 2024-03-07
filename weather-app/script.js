1d2412ec670daf1bae943e427bae0b2475322c77
lemodoescoding <119138020+lemodoescoding@users.noreply.github.com>
2024-03-08 00:03:46 +0700

    Added script.js to weather-app

--- weather-app/script.js
+++ weather-app/script.js
@@ -1,2 +1,14 @@
 const apiKey = ""
 const apiURL = ""
+
+const searchButton = document.querySelector("#searchBtn")
+
+searchButton.addEventListener("click", function(){
+    let searchCity = document.querySelector("#searchCity")
+    if(searchCity.value === ""){
+        return alert("Please input atleast one city...")
+    }
+    
+    console.log(searchCity.value)
+    searchCity.value = ""
+})
